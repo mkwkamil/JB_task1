974fda1cdcafb8e11f13bcbc538a1c6ce672e39e
mkwkamil <kamil.porebski@icloud.com>
2025-09-24 10:37:03 +0200

    Interfaces and Enums

---
+++ JB_task1/Expression.cs
@@ -0,0 +1,29 @@
+namespace JB_task1;
+
+public interface IExpression { }
+
+public interface IConstantExpression : IExpression
+{
+    int Value { get; }
+}
+
+public interface IVariableExpression : IExpression
+{
+    string Name { get; }
+}
+
+public interface IBinaryExpression : IExpression
+{
+    IExpression Left { get; }
+    IExpression Right { get; }
+    OperatorSign Sign { get; }
+}
+
+public interface IFunction : IExpression
+{
+    FunctionKind Kind { get; }
+    IExpression Argument { get; }
+}
+
+public enum FunctionKind { Sin, Cos, Max }
+public enum OperatorSign { Plus, Minus, Multiply, Divide }
